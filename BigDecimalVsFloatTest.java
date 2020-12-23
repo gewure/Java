4311c4ef538374904f6acf16614e6c849e5d19f5
Jasper Reichardt <jasperreichardt@gmail.com>
2020-12-23 21:03:16 +0100

    Create BigDecimalVsFloatTest.java

---
+++ BigDecimalVsFloatTest.java
@@ -0,0 +1,43 @@
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Comparator;
+
+// results:
+// Double is about 8x to 12x times faster sorted in an ArrayList 
+public class BigDecimalVsFloatTest {
+
+    @Test
+    public void bigIntTimeCostTest() {
+        long start = System.nanoTime();
+        BigDecimal biige = new BigDecimal(334232323534.034434343468958030493d);
+
+        ArrayList<BigDecimal> biggis = new ArrayList<>();
+
+        for(int i = 0; i < 50; i++) {
+            biggis.add(biige.add(new BigDecimal(234.0)));
+        }
+
+        biggis.sort(Comparator.comparing(BigDecimal::doubleValue));
+
+        System.out.println(System.nanoTime()-start);
+    }
+
+    @Test
+    public void doubleTimeCostTest() {
+        long start = System.nanoTime();
+        double biige = 334232323534.034434343468958030493d;
+
+        ArrayList<Double> biggis = new ArrayList<>();
+
+        for(int i = 0; i < 50; i++) {
+            biggis.add(biige+234.0);
+        }
+
+        biggis.sort(Comparator.comparing(Double::doubleValue));
+
+        System.out.println(System.nanoTime()-start);
+    }
+}
